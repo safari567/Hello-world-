a653be4d70bcedbeb9c42dd6f0f26eeec975e11b
safari567 <104107515+safari567@users.noreply.github.com>
2022-04-21 02:34:19 +0300

    Edit beta version 0.2

--- Hello, world!/Hello, world.py
+++ Hello, world!/Hello, world.py
@@ -1,2 +1,7 @@
 name = input("Your name is ")
-print("Hello, world! My name is", name)
+superpower = input("Your superpower ")
+numbers1 = input("Random numbers 1: ")
+numbers2 = input("Random numbers 2: ")
+numbers3 = numbers1 * numbers2
+print("Hello", name)
+print("I am machine. I can", numbers1, "*", numbers2, "=" numbers3 ".  Im f@ck your", superpower)
